3343e44d043704c254f14d741cde69ef9c2d49f6
remnantsofred <remnantsofred@gmail.com>
2022-10-12 10:17:15 -0700

    finished battleship project :D

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,71 @@
 class Board
-  
+    attr_reader :size
+
+    def initialize(n)
+        @grid = Array.new(n) {Array.new(n, :N)}
+        @size = n * n
+    end
+
+    def [](arr)
+        @grid[arr[0]][arr[1]]
+    end
+
+    def []=(position, val)
+        @grid[position[0]][position[1]] = val
+    end
+
+    def num_ships
+        count = 0
+        one_d = @grid.flatten
+        one_d.each { |ele| count += 1 if ele == :S }
+        count
+    end
+
+    def attack(position)
+        if self[position] == :S 
+            self[position] = :H
+            p 'you sunk my battleship!'
+            return true
+        else
+            self[position] = :X
+            return false
+        end
+    end
+
+    def place_random_ships
+        num = @size / 4
+        while self.num_ships < num
+            random_position = [rand(0..@grid.length-1), rand(0..@grid.length-1)]
+            self[random_position] = :S
+        end
+    end
+
+    def hidden_ships_grid
+        hidden_grid = @grid.map do |subarr|
+            subarr.map do |ele|
+                if ele == :S 
+                    :N 
+                else
+                    ele
+                end
+            end
+        end
+        hidden_grid
+    end
+
+    def self.print_grid(arr)
+        arr.each do |subarr|
+            puts subarr.join(" ")
+        end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+    
+    def print
+        Board.print_grid(hidden_ships_grid)
+    end
+
 end
+
--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,16 @@
 class Player
 
+    # def initialize
+    #     @player
+    # end
+
+
+    def get_move
+        p 'enter a position with coordinates separated with a space like `4 7`'
+        move = gets.chomp
+        coordinates = move.split(" ")
+        coordinates.map! { |num| num.to_i }
+    end
+
+
 end
